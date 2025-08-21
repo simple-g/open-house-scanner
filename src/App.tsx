bc5612747fb46f97428e85a43a02bebc7f904ced
simple-g <67791330+simple-g@users.noreply.github.com>
2025-08-20 21:33:42 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import CreateEvent from "./pages/CreateEvent";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import EventSuccess from "./pages/EventSuccess";
 import NotFound from "./pages/NotFound";
+
+/* 👇 add these three imports */
+import Admin from "./pages/Admin";
+import Poster from "./pages/Poster";
+import AdminSignins from "./pages/AdminSignins";
 
 const queryClient = new QueryClient();
 
@@ -19,12 +25,19 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          {/* your existing routes */}
           <Route path="/" element={<Index />} />
           <Route path="/create-event" element={<CreateEvent />} />
           <Route path="/e/:eventId" element={<SignIn />} />
           <Route path="/event/:eventId/success" element={<EventSuccess />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* 👇 new backend-powered routes */}
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/poster/:id" element={<Poster />} />
+          <Route path="/admin/signins/:id" element={<AdminSignins />} />
+
+          {/* keep catch-all last */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
