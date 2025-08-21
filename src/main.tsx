a372a77bb6c63c136b87aa579a72a7e60f653526
simple-g <67791330+simple-g@users.noreply.github.com>
2025-08-20 21:46:43 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import App from './App'
 import './index.css'
 
-import App from './App'
-import Admin from './pages/Admin'
-import Poster from './pages/Poster'
-import SignIn from './pages/SignIn'
-import AdminSignins from './pages/AdminSignins'
+const root = document.getElementById('root') as HTMLElement
 
-const rootEl = document.getElementById('root')!
-createRoot(rootEl).render(
+createRoot(root).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Home / landing */}
-        <Route path="/" element={<App />} />
-
-        {/* Admin flows */}
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/signins/:id" element={<AdminSignins />} />
-
-        {/* Public pages */}
-        <Route path="/poster/:id" element={<Poster />} />
-        <Route path="/sign-in" element={<SignIn />} />
-
-        {/* Fallback: anything unknown goes home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
